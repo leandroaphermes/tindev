2eaee53ec8352a57f169c04553e9be935db87c88
Leandro Hermes <leandro.aparecido.hermes@gmail.com>
2019-08-08 00:23:20 -0300

    Testando ignore

---
+++ backend/configs/mongodb.js
@@ -0,0 +1,4 @@
+ mongoDB = "mongodb+srv://devdevdev:devdevdev@dev-mtjzg.gcp.mongodb.net/tindev?retryWrites=true&w=majority";
+
+
+ module.exports = mongoDB;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const routes = require("./routes");
+const mongodbconfig = require("../configs/mongodb");
 
 const server = express();
+
+mongoose.connect(mongodbconfig, {
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
